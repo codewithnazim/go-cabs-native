f181ff6379c63eea052fa15a58ce57a7dd0a833c
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-03-24 17:08:10 +0530

    config - babel file update to handle env variables

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ["module:@react-native/babel-preset"],
+  plugins: [
+    [
+      "module:react-native-dotenv",
+      {
+        "envName": "APP_ENV",
+        "path": ".env",
+        "safe": false,
+        "allowUndefined": true
+      }
+    ]
+  ]
 };
