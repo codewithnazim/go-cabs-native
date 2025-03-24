feb9085d020c51dfe0e0e42066a0c80dcb1ad9d6
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-03-24 17:19:51 +0530

    chore - app tsx chore

--- App.tsx
+++ App.tsx
@@ -1,30 +1,34 @@
-import React from 'react';
-import * as eva from '@eva-design/eva';
-import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
-import { StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { customTheme } from './src/theme/custom-theme';
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import RootNavigator from './src/routes/RootNavigator';
-import { backgroundPrimary } from './src/theme/colors';
-import { customMapping } from './src/theme/customMapping';
-
+import React from "react";
+import * as eva from "@eva-design/eva";
+import {ApplicationProvider, IconRegistry} from "@ui-kitten/components";
+import {StyleSheet} from "react-native";
+import {SafeAreaView} from "react-native-safe-area-context";
+import {customTheme} from "./src/theme/custom-theme";
+import {EvaIconsPack} from "@ui-kitten/eva-icons";
+import RootNavigator from "./src/routes/RootNavigator";
+import {backgroundPrimary} from "./src/theme/colors";
+import {customMapping} from "./src/theme/customMapping";
+import {RecoilRoot} from "recoil";
 const App = () => (
-  <>
+  <RecoilRoot>
     <IconRegistry icons={EvaIconsPack} />
-    <ApplicationProvider {...eva} theme={{ ...eva.light, ...customTheme }} customMapping={customMapping}>
+    <ApplicationProvider
+      {...eva}
+      theme={{...eva.light, ...customTheme}}
+      customMapping={customMapping}>
       <SafeAreaView style={styles.container}>
         <RootNavigator />
       </SafeAreaView>
     </ApplicationProvider>
-  </>
+  </RecoilRoot>
 );
+
 export default App;
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: backgroundPrimary,
     flex: 1,
-    color: '#fff',
+    color: "#fff",
   },
 });
