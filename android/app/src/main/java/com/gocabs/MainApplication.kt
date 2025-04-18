a7c6cf7eed2b204d459256a0b0f427473a43cf20
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-04-19 02:55:46 +0530

    build - build error fix for android apk

--- android/app/src/main/java/com/gocabs/MainApplication.kt
+++ android/app/src/main/java/com/gocabs/MainApplication.kt
@@ -11,6 +11,7 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
+import com.lugg.RNCConfig.RNCConfigPackage
 
 class MainApplication : Application(), ReactApplication {
 
@@ -19,12 +20,12 @@ class MainApplication : Application(), ReactApplication {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
+              add(RNCConfigPackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
 
-        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+        override fun getUseDeveloperSupport(): Boolean = true
 
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
