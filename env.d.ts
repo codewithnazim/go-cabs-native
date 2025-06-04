a8894f4177c2e817cb82e728a54cf5e72c3a1827
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-06-04 16:29:51 +0530

    chore: update environment configuration for development and production, add socket server URLs, and modify Android build properties

--- env.d.ts
+++ env.d.ts
@@ -1,4 +1,6 @@
 declare module "@env" {
   export const FIREBASE_WEB_CLIENT_ID: string;
   export const GOOGLE_MAPS_API_KEY_BOOKRIDE: string;
+  export const SOCKET_SERVER_URL : string;
+  export const EXPO_PUBLIC_SOCKET_SERVER_URL : string;
 }
